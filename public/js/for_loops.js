7141573e2ab693742d0b76675d13f79ccf51a51b
memohernandez <hernandez.a.memo@gmail.com>
2017-06-15 17:06:05 -0500

    Added number 3 to For Loops exercise

--- public/js/for_loops.js
+++ public/js/for_loops.js
@@ -1,5 +1,13 @@
 var number = prompt("Enter a number between 1 and 10");
 
-for (var i = 1; i<=10; i++){
+for (var i = 1; i <=10; i++){
 	console.log(number + "x" + i + "=" + number * i );
+}
+
+for (var i = 1; i <= 10; i++){
+	var numbers = Math.floor(Math.random() * 180) + 20;
+	if (numbers % 2 === 0) {
+		console.log("Your number is even" + numbers)
+		console.log("Your number is odd" + numbers)
+	}
 }
