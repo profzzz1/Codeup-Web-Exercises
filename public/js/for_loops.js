4f9d0d457f4feb95e05ff9bacc6333efbba7c18d
memohernandez <hernandez.a.memo@gmail.com>
2017-06-16 08:55:34 -0500

    Finished For Loops exercises and finished Break and Continue exercises

--- public/js/for_loops.js
+++ public/js/for_loops.js
@@ -11,3 +11,29 @@ for (var i = 1; i <= 10; i++){
 		console.log("Your number is odd" + numbers)
 	}
 }
+
+// output should match this pattern
+ // 1
+ // 22
+ // 333
+ // 4444
+ // 55555
+ // 666666
+ // 7777777
+ // 88888888
+ // 999999999
+ // 0000000000
+ var stringNumber;
+ 
+ for(var i = 1; i <= 10; i++) {
+     stringNumber = i.toString();
+ 
+     stringNumber = stringNumber.substr(stringNumber.length -1);
+     
+     console.log(stringNumber.repeat(i));
+ }
+ 
+ 
+ for(var i = 100; i >= 5; i = i - 5) {
+     console.log(i);
+ } 
